ec3efb87a5b915cca0d4c82aaf1608789990896f
ryuon <dododosa14@gmail.com>
2019-06-23 16:29:35 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello")
+	})
+
+	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
+}
