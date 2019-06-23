45cc7fd6bdf680b60b563aaf62ef930db8ac7b2f
ryuon <dododosa14@gmail.com>
2019-06-23 17:21:43 +0900

    test

--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 	"os"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello")
