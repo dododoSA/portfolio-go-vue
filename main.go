da02e5b6a90aa219dbadce70db969b7e01364bd8
ryuon <dododosa14@gmail.com>
2019-06-23 23:00:46 +0900

    テスト

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-
-	e.GET("/", func(c echo.Context) error {
+	e.File("/", "./front/index.html")
+	e.GET("/hello", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello")
 	})
 
