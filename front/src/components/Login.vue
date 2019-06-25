d9fbe001404f2bc0df6d40b03962b8a7195df46b
ryuon <dododosa14@gmail.com>
2019-06-26 01:42:03 +0900

    サインアップ画面とログイン画面を追加

---
+++ front/src/components/Login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="login">
+        <h2>Log in</h2>
+        <input type="text" placeholder="Username" v-model="username">
+        <input type="password" placeholder="Password" v-model="password">
+        <button @click="logIn">ログイン</button>
+        <p>アカウントを持ってない方は
+            <router-link to="/signup">こちら</router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data () {
+        return {
+            username: '',
+            password: ''
+        }
+    },
+    methods: {
+        logIn: function(){
+            if (newuser.username != '' && newuser.password != ''){
+                this.$http.post('https://portfolio-go-vue.herokuapp.com/login', newuser, function(data, status, request){
+                    console.log("login success")
+                    console.log(status)
+                    this.$router.push({name: 'HelloWorld'})
+                }).error(function (data, status, request){
+                    console.log("login failed")
+                })
+            }
+        }
+    }
+}
+</script>
---
+++ front/src/components/Signup.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="signup">
+        <h2>Sign up</h2>
+        <input type="text" placeholder="Username" v-model="username">
+        <input type="password" placeholder="Password" v-model="password">
+        <button @click="signUp">登録</button>
+        <p>アカウントを持っている方は
+            <router-link to="/login">こちら</router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Signup',
+    data () {
+        return {
+            newuser:{
+                username: '',
+                password: ''
+            }
+        }
+    },
+    methods: {
+        signUp: function() {
+            if (newuser.username != '' && newuser.password != ''){
+                this.$http.post('https://portfolio-go-vue.herokuapp.com/signup', newuser, function(data, status, request){
+                    console.log("usercreate success")
+                    console.log(status)
+                    this.$router.push({name: 'HelloWorld'})
+                }).error(function (data, status, request){
+                    console.log("usercreate failed")
+                })
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
