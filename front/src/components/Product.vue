53687f750489d3c92a37ab314705317ed008bb54
ryuon <dododosa14@gmail.com>
2019-07-01 13:41:12 +0900

    no message

--- front/src/components/Product.vue
+++ front/src/components/Product.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="products">
-			<router-link to="{ name: 'products-new', path: '/users/' + id + '/products/new' }" v-if="currentuserId == id">新規投稿</router-link>
+			<router-link to="{ name: 'products-new', params:{id: id} }" v-if="currentuserId == id">新規投稿</router-link>
         <h2>作品一覧</h2>
         <ul v-for="product in products" v-bind:key="product.id">
             <li>
