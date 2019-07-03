41b6478c1bcf990c1ecbbe808b46ffbc5432d3d0
ryuon <dododosa14@gmail.com>
2019-07-03 22:10:14 +0900

    FIX

--- front/src/components/Signup.vue
+++ front/src/components/Signup.vue
@@ -32,7 +32,7 @@ export default {
                 })
                 .then(function(response){
                     console.log(response)
-
+                    _this.$store.dispatch('GET_ME')
                     _this.$router.push('/users/' + this.$store.state.currentuserId)
                 })
                 .catch(function(error){
