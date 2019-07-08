f90afb1ed297ae02944686eabc73747760102e66
ryuon <dododosa14@gmail.com>
2019-07-08 22:12:26 +0900

    動かない

--- front/src/main.js
+++ front/src/main.js
@@ -8,6 +8,8 @@ import store from './store'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue)
 
 Vue.use(Vuex)
 Vue.config.productionTip = false
